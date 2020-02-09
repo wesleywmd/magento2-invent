ec9b8e639f7544a3dd102ee544ddd83e950def7a
wesleywmd <therealwesleywmd@gmail.com>
2020-02-09 16:48:38 -0600

    fix multi-file components partially creating needed files and dying part way through the list

--- Model/Component/BaseComponent.php
+++ Model/Component/BaseComponent.php
@@ -17,22 +17,27 @@ class BaseComponent implements ComponentInterface
 
     public function addToModule(DataInterface $data)
     {
+        $this->checkRenderersCanCreate($data);
         foreach ($this->renderers as $renderer) {
-            if (!is_a($renderer, RendererInterface::class)) {
-                throw new \Exception(get_class($renderer) .' does not implement '. RendererInterface::class);
-            }
-
             $location = $renderer->getPath($data);
-            if (is_file($location) && !$renderer->getForce()) {
-                throw new \Exception('File already exists at: '.$location);
-            }
-            
             $dirname = dirname($location);
             if (!is_dir($dirname)) {
                 mkdir($dirname, 0777, true);
             }
-            
             file_put_contents($location, $renderer->getContents($data), LOCK_EX);
+        }
+    }
+
+    private function checkRenderersCanCreate(DataInterface $data)
+    {
+        foreach ($this->renderers as $renderer) {
+            if (!is_a($renderer, RendererInterface::class)) {
+                throw new \Exception(get_class($renderer) .' does not implement '. RendererInterface::class);
+            }
+            $location = $renderer->getPath($data);
+            if (is_file($location) && !$renderer->getForce()) {
+                throw new \Exception('File already exists at: '.$location);
+            }
         }
     }
 }
